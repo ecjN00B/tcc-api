332185b50638114292a36e5f046053e9f46640a0
Elias Jr <eliasdecarvalhojunior@hotmail.com>
2020-10-03 20:43:42 -0300

    fix: make controller returns error message instead of name

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,9 +1,10 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+import fs from 'fs'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts')
-    .map(async file => (await import(`../../../${file}`)).default(router) )
+  fs.readdirSync(`${__dirname}/../routes`).map(async file => {
+    (await import(`../routes/${file}`)).default(router)
+  })
 }
